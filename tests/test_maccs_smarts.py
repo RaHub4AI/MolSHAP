aceb84265e44e41f8b9da292bcbd9649f5572e21
Christian W. Feldmann <christian.w.feldmann@gmail.com>
2025-09-12 13:44:43 +0200

    Add MACCs

---
+++ tests/test_maccs_smarts.py
@@ -0,0 +1,19 @@
+"""Test if SMARTs are valid."""
+
+import unittest
+
+from molshap.maccs_smarts import MACCS_KEYS
+from rdkit import Chem
+
+
+class TestSMARTSPattern(unittest.TestCase):
+    def test_validity(self) -> None:
+        """Test is SMARTS are valid."""
+        for key, smarts in MACCS_KEYS.items():
+            with self.subTest(f"Testing key: {key}"):
+                smarts_obj = Chem.MolFromSmarts(smarts)
+                self.assertIsNotNone(smarts_obj)
+
+
+if __name__ == '__main__':
+    unittest.main()
