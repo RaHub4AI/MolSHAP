278742ceadf5aaa390320f10746b1873d7f3b7e9
Esben Jannik Bjerrum <esben.bjerrum@abbvie.com>
2025-09-12 11:27:22 +0200

    draft dataset module

---
+++ src/molshap/dataset.py
@@ -0,0 +1,32 @@
+import os
+import pathlib
+import pandas as pd
+
+this_script_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_slc6a4_df():
+    csv_file = pathlib.Path(f"{this_script_dir}/ressources/SLC6A4_active_excape_export.csv")
+    if not os.path.exists(csv_file):
+        import urllib.request
+        url = "https://ndownloader.figshare.com/files/25747817"
+        urllib.request.urlretrieve(url, csv_file)
+
+    df = pd.read_csv(csv_file)
+    return df
+
+
+def get_logp_df():
+    csv_file = pathlib.Path(f"{this_script_dir}/ressources/logP.csv")
+    if not os.path.exists(csv_file):
+        import urllib.request
+        url = "https://zenodo.org/records/14333718/files/logP.csv"
+        urllib.request.urlretrieve(url, csv_file)
+
+    df = pd.read_csv(csv_file)
+    return df
+
+
+if __name__ == "__main__":
+    #print(get_slc6a4_df().head())
+    print(get_logp_df().head())
---
+++ src/molshap/ressources/readme.md
@@ -0,0 +1,2 @@
+#This directory is intended for data that should not be commited, but are required for the examples
+as example downloaded .csv files (see ../dataset.py)
